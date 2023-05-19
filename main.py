cb515f1084f6dd31a7dec6d9c531fc3d878bf3a5
AnirudhReddy <44012589+anirudh89201@users.noreply.github.com>
2023-05-20 01:09:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ i = 0
 slide = cv.imread("Slides/"+img_list[i])
 slide = cv.resize(slide,(800,600))
 v = cv.VideoCapture(0)
-
-detector = HandDetector(maxHands = 1, detectionCon=0.8)
-
+detector = HandDetector(maxHands = 2, detectionCon=0.8)
 while True:
-    
     ret,frame = v.read()
     flipped_frame = cv.flip(frame,1)  #flips horizontally
     hands,flipped_frame = detector.findHands(flipped_frame)
+    if hands:
+        hand = hands[0]
+        fingers = detector.fingersUp(hand)
+        print(fingers)
+                
     small_v = cv.resize(flipped_frame,(150,100))
+    cv.imshow("frame",flipped_frame);
     y = slide.shape[0] - small_v.shape[0] - 10   
     x = slide.shape[1] - small_v.shape[1] - 10   
     slide[y:y+small_v.shape[0],x:x+small_v.shape[1]] = small_v    # Selecting the region on the background image for placing video. 
