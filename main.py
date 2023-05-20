6d1f3d846c7ab58c6a0b0eaca4d9d2632efbbcce
CHAITANYA000003 <133523237+CHAITANYA000003@users.noreply.github.com>
2023-05-20 10:33:33 +0530

    Add Annotations

--- main.py
+++ main.py
@@ -1,98 +1,85 @@
 import cv2 as cv
-import os
 import numpy as np
+import os
 from cvzone.HandTrackingModule import HandDetector
-
-width, height = 800, 600
-
 img_list = os.listdir("Slides")
 print(img_list)
 i = 0
 slide = cv.imread("Slides/"+img_list[i])
-slide = cv.resize(slide,(800,600))
+width,height = 800,600
+slide = cv.resize(slide,(width,height))
+# slide = cv.resize(slide,cap.set)
 v = cv.VideoCapture(0)
 detector = HandDetector(maxHands = 1, detectionCon=0.8)
-lineThreshold = 250
+lineThreshold = height//3
 
-clickCounter = 0 
-clickLimit = 5
-clickCheck = False
-
-
+# clickCounter = 0 
+# clickLimit = 5
+# clickCheck = False
+annotations = []
 while True:
     ret,frame = v.read()
     flipped_frame = cv.flip(frame,1)  #flips horizontally
-    cv.line(flipped_frame,(0,lineThreshold),(800,lineThreshold),(0,255,0),1)
+    cv.line(flipped_frame,(0,lineThreshold),(width,lineThreshold),(0,255,0),1)
     hands,flipped_frame = detector.findHands(flipped_frame)
-    
-    cv.imshow("Cam", flipped_frame)
-    if cv.waitKey(1) & 0xFF == ord("q"):
-                break
-    
-    if hands and clickCheck==False:
-
-        
+    # if hands and clickCheck==False:
+    v_width , v_height = 150,100
+    small_v = cv.resize(flipped_frame,(v_width,v_height))
+    # cv.imshow("frame",flipped_frame);
+    y = slide.shape[0] - small_v.shape[0] - 10   
+    x = slide.shape[1] - small_v.shape[1] - 10   
+    slide[y:y+small_v.shape[0],x:x+small_v.shape[1]] = small_v    # Selecting the region on the background image for placing video. 
+    if hands:
         hand = hands[0]
         fingers = detector.fingersUp(hand)
         print(fingers)
-
-
         cx,cy = hand['center']
-
-        lmList = hand["lmList"]  # for pointer and drawing
-        # Setting the pointer movement on the right hand side
-        xvalue = int(np.interp(lmList[8][0], [300, 750], [0, 800]))
-        yvalue = int(np.interp(lmList[8][1], [50, 500], [0, 600]))
-        indexFinger = xvalue, yvalue
-
+        lmlist = hand['lmList']
+        # xVal = int(np.interp(lmlist[8][0],[width//2,width],[0,width]))
+        # yVal = int(np.interp(lmlist[8][1],[150,height-150],[0,height]))
+        xVal = int(np.interp(lmlist[8][0], [width//2, width], [0, width+width]))
+        yVal = int(np.interp(lmlist[8][1], [0, height//2], [0, height]))
+        indexFinger = xVal,yVal
         if cy <= lineThreshold:
+            # clickCheck = True
             if fingers == [1,0,0,0,0]:
-                clickCheck = True
-                # print('Left')
+                print('Left')
                 
                 if i>0:
                     i-=1 
 
                     # print(i)
                     slide = cv.imread("Slides/"+img_list[i])
-                    slide = cv.resize(slide,(800,600))
+                    slide = cv.resize(slide,(width,height))
             
                 else:
                     print("This is the beginning slide of this presentation.")
                 
             if fingers == [0,0,0,0,1]:
-                # print('Right')
+                print('Right')
+                # clickCheck = True
                 if i< len(img_list)-1:
-                    clickCheck = True
                     i+=1 
                     slide = cv.imread("Slides/"+img_list[i])
-                    slide = cv.resize(slide,(800,600))
+                    slide = cv.resize(slide,(width,height))
                 else:
                     print("This is the end of the presentation.")
-            
+        # if fingers == [0,1,1,0,0]:
+            # cv.circle(slide,indexFinger,5,(0,0,255),cv.FILLED)
+        if fingers == [0,1,0,0,0]:
+            cv.circle(slide,indexFinger,5,(0,0,255),cv.FILLED)
+            annotations.append(indexFinger)
 
-            # Pointer
-        if fingers == [0, 1, 1, 0, 0]:
-            x = fingers[1] * slide.shape[1]
-            y = fingers[2] * slide.shape[0]
-            # cv.circle(slide, indexFinger, 12, (0, 0, 255), cv.FILLED)
-            cv.circle(slide, indexFinger, 12, (0, 0, 255), cv.FILLED)
-
-    cv.imshow("Presentation slides", slide)
-
-    if clickCheck:
-        clickCounter+=1 
-        if clickCounter > clickLimit:
-            clickCounter = 0 
-            clickCheck = False 
+        for j in range(len(annotations)):
+            if j!=0:
+                cv.line(slide,annotations[j-1],annotations[j],(0,255,0),12)
+    # if clickCheck:
+    #     clickCounter+=1 
+    #     if clickCounter > clickLimit:
+    #         clickCounter = 0 
+    #         clickCheck = False 
                 
 
-                
-    small_v = cv.resize(flipped_frame,(150,100))
-    # cv.imshow("frame",flipped_frame);
-    y = slide.shape[0] - small_v.shape[0] - 10   
-    x = slide.shape[1] - small_v.shape[1] - 10   
-    slide[y:y+small_v.shape[0],x:x+small_v.shape[1]] = small_v    # Selecting the region on the background image for placing video. 
     key = cv.waitKey(1)
     cv.imshow("Presentation Slides",slide)
     if key==ord('q'):
